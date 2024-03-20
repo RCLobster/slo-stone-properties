5e6ea1aae564ff70dbca928a2168ed8733083c7e
Zachary Roy <vgd199@gmail.com>
2024-03-20 14:52:29 -0700

    made a new header, figured out how to center the navbar

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+import AppNavbar from "./Navbar";
+
+const Header = () => {
+
+  return (
+    <header>
+    <h1>Slo Stone Properties</h1>
+    <AppNavbar />
+    </header>
+  );
+};
+
+export default Header;
