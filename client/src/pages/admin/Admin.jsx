50ca7be11a752f58433ba82f4dedb0cdf016850d
mgberrysd <mgberrysd@gmail.com>
2024-03-25 14:15:13 -0700

    reverted admin page

--- client/src/pages/admin/Admin.jsx
+++ client/src/pages/admin/Admin.jsx
@@ -72,17 +72,17 @@ function Admin() {
             </div>
             <Container>
                 <Row>
-                    {groupData.map((group, index) => {
+                    {groupData.map((group) => {
                         return (
                             <Col md="9">
-                                <Card key={index} border='dark'>
+                                <Card key={group._id} border='dark'>
                                     <Card.Body>
                                         <Card.Title><strong>Group Name: </strong>{group.groupName}</Card.Title>
                                         <Card.Text><strong>Property:</strong> {group.property}</Card.Text>
                                         <Card.Text><strong>Group size:</strong> {group.applicantCount}</Card.Text>
-                                        {group.applicants.map((applicant, index) => {
+                                        {group.applicants.map((applicant) => {
                                             return (
-                                                <Col md="4" key={index}>
+                                                <Col md="4" key={applicant._id}>
                                                     <Card.Text><strong>Applicant Name:</strong> {applicant.name}</Card.Text>
                                                     <Card.Text><strong>School:</strong> {applicant.school}</Card.Text>
                                                     <Card.Text><strong>Anticipated Graduation:</strong> {applicant.graduation}</Card.Text>
